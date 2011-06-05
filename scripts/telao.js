3d7169ab89114f0f3fcc67b3c744e9abb26fed76
Alberto Chvaicer <achvaicer@gmail.com>
2011-06-05 09:54:01 -0300

    Pegando a última foto e último vídeo enviados

--- scripts/telao.js
+++ scripts/telao.js
@@ -1,12 +1,39 @@
 var fjson = "https://picasaweb.google.com/data/feed/base/user/achvaicer/albumid/5588740059809037009?alt=json&kind=photo&authkey=Gv1sRgCPXT_IDYp8OfGA&hl=en_US";
-var vjson = "ttp://gdata.youtube.com/feeds/users/achvaicer/uploads?alt=json&format=5";
+var vjson = "http://gdata.youtube.com/feeds/users/machinima/uploads?alt=json&format=5";
 
 $(document).ready(function() {
-	$.getJSON(fjson,
-		function(data) {
-			var f = data.feed.entry;
-			if (f && f.length)
-				$("#foto").attr("src", f[f.length -1].content.src);
-			console.log(data);
-		});
+//	loadPhoto();
+	loadVideo();
 });
+
+function doAjax(url, cb) {
+	$.getJSON(url, function(data) {cb(data);});
+}
+
+function showPhoto(src) {
+	$("#foto").attr("src", src).show();
+	$("#video").hide();
+}
+
+function showVideo(src) {
+	$("#video").attr("src", "http://www.youtube.com/embed/" + src + "?rel=0&autoplay=1&controls=0").show();
+	$("#foto").hide();
+
+}
+
+function loadPhoto() {
+	doAjax(fjson, function(data) {
+		var f = data.feed.entry;
+		if (f && f.length)
+			showPhoto(f[f.length -1].content.src);
+	});
+}
+
+function loadVideo() {
+	doAjax(vjson, function(data) {
+		var v = data.feed.entry;
+		if (v && v.length)
+			showVideo($.url.setUrl(v[v.length-1].link[0].href).param("v"));
+		console.log(data);		
+	});
+}
