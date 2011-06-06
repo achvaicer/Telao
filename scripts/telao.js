298e20e5c1da1f0a975c77410f965f7f08a1d5c4
Alberto Chvaicer <achvaicer@gmail.com>
2011-06-05 22:52:56 -0300

    Mostrando fotos e vídeos com intervalo de 1 minuto

--- scripts/telao.js
+++ scripts/telao.js
@@ -1,11 +1,12 @@
 var fjson = "https://picasaweb.google.com/data/feed/base/user/achvaicer/albumid/5588740059809037009?alt=json&kind=photo&authkey=Gv1sRgCPXT_IDYp8OfGA&hl=en_US";
-var vjson = "http://gdata.youtube.com/feeds/users/machinima/uploads?alt=json&format=5";
+var vjson = "http://gdata.youtube.com/feeds/users/achvaicer/uploads?alt=json&format=5";
 
 var items = [];
 
 $(document).ready(function() {
 	loadPhoto();
 	loadVideo();
+	setInterval(showLessViewed, 60000);
 });
 
 function doAjax(url, cb) {
@@ -13,8 +14,11 @@ function doAjax(url, cb) {
 }
 
 function showLessViewed() {
+	if (!items.length) return;
 	items.sort(function (a,b) { return a.viewed < b.viewed ? -1 : 1 });
-	
+	var first = items[0];
+	first.show(first.src);
+	first.viewed++;
 }
 
 function showPhoto(src) {
@@ -31,11 +35,11 @@ function showVideo(src) {
 function loadPhoto() {
 	doAjax(fjson, function(data) {
 		console.log(data);
-		var f = data.feed.entry;
+		var f = data.feed.entry || [];
 		for (var i=0; i < f.length; i++) {
 			var src = f[i].content.src;
 			if (!items.filter(function(e) { return e.type == "photo" && e.src == src }).length)
-				items.push({"src":src, type:"photo", viewed:0});
+				items.push({"src":src, type:"photo", viewed:0, show:showPhoto});
 		}
 		setTimeout(loadPhoto, 60000);		
 	});
@@ -44,11 +48,11 @@ function loadPhoto() {
 function loadVideo() {
 	doAjax(vjson, function(data) {
 		console.log(data);
-		var v = data.feed.entry;
+		var v = data.feed.entry || [];
 		for (var i =0; i < v.length; i++) {
-			var ytid = $.url.setUrl(v[i].link[0].href).param("v");
-			if (!items.filter(function(e) { return e.type == "video" && e.id == ytid }).length)
-				items.push({id:ytid, type:"video", viewed:0});
+			var src = $.url.setUrl(v[i].link[0].href).param("v");
+			if (!items.filter(function(e) { return e.type == "video" && e.src== src }).length)
+				items.push({"src":src, type:"video", viewed:0, show:showVideo});
 
 		}
 		setTimeout(loadVideo, 60000);
